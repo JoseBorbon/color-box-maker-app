02f70986d666aebb3856485afba3369d5562f7d3
JoseBorbon <joserborbon@gmail.com>
2021-10-23 06:52:21 -0400

    removed un-needed styles

--- src/NewBoxForm.jsx
+++ src/NewBoxForm.jsx
@@ -1,8 +1,43 @@
 import React, { Component } from 'react';
 
 class NewBoxForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { width: '', height: '', bgColor: '' };
+  }
+
+  handleChange(evt) {
+    this.setState({ [evt.target.name]: evt.target.value });
+  }
   render() {
-    return <div></div>;
+    return (
+      <form>
+        <label htmlFor="width">Width: </label>
+        <input
+          type="text"
+          id="width"
+          name="width"
+          onChange={this.handleChange}
+        />
+        <br />
+        <label htmlFor="height">Height: </label>
+        <input
+          type="text"
+          id="height"
+          name="height"
+          onChange={this.handleChange}
+        />
+        <br />
+        <label htmlFor="bgColor">Background-color: </label>
+        <input
+          type="text"
+          id="bgColor"
+          name="bgColor"
+          onChange={this.handleChange}
+        />
+        <br />
+      </form>
+    );
   }
 }
 
